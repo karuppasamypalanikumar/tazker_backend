b258342a2e97a0e7e8b1644f9194b68f1ae6e619
Karuppasamy Palanikumar <karuppasamypalanikumar@gmail.com>
2023-07-30 18:21:25 +0530

    feat(base): database Local Added based on the Environment

--- tazker/settings/dev.py
+++ tazker/settings/dev.py
@@ -1,19 +1,30 @@
 import os
 from tazker.settings.base import *
+from dotenv import load_dotenv
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+load_dotenv()
+
 # settings_dev.py
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        "NAME": 'tazker',
-        "USER": 'karuppasamypalanikumar',
-        "PASSWORD": '',
-        "HOST": "localhost",
-        "PORT": 5432,
+if os.environ.get('DB') == "POSTGRES":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            "NAME": 'tazker',
+            "USER": 'karuppasamypalanikumar',
+            "PASSWORD": '',
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'tazker.sqlite3'
+        }
+    }
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- tazker/settings/prd.py
+++ tazker/settings/prd.py
@@ -1,17 +1,30 @@
+import os
 from tazker.settings.base import *
+from dotenv import load_dotenv
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        "NAME": 'postgres',
-        "USER": 'postgres',
-        "PASSWORD": 'Karuppasamy22',
-        "HOST": "tazker-dev-new.cauiydqtyks7.eu-north-1.rds.amazonaws.com",
-        "PORT": 5432,
+load_dotenv()
+
+# settings_dev.py
+if os.environ.get('DB') == "POSTGRES":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            "NAME": 'tazker',
+            "USER": 'karuppasamypalanikumar',
+            "PASSWORD": '',
+            "HOST": "localhost",
+            "PORT": 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'tazker.sqlite3'
+        }
+    }
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
